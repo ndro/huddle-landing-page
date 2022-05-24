f24c4004d996208daa79f5068d686bee42e4ab50
hendro <hendro.tb@gmail.com>
2022-05-24 16:56:35 +0700

    tailwind.config.js: add theme colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'soft-magenta': 'hsl(300, 69%, 71%)'
+      }
+    },
   },
   plugins: [],
 }
